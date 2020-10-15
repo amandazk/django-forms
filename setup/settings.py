75192a58ba8d3bafdae3fd2e0c2cf5000bc44bfa
amandazk <zacharukaz@gmail.com>
2020-10-15 09:41:05 -0300

    Calendario nas datas

--- setup/settings.py
+++ setup/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     #Meus apps
     'passagens',
+    'tempus_dominus',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPUS_DOMINUS_LOCALIZE = True
