d7d47ff5aac0de94107e25f27f70e54b5b4fe7e4
amandazk <zacharukaz@gmail.com>
2020-10-18 11:09:38 -0300

    Mudança model classe_viagem

--- passagens/models/classe_viagem.py
+++ passagens/models/classe_viagem.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class ClasseViagem(models.TextChoices):
-        ECONOMICA = 'ECON', _('Econômica')
-        EXECUTIVA = 'EXEC', _('Executiva')
-        PRIMEIRA_CLASSE = 'PRIC', _('Primeira Classe')
+        ECONOMICA = 'ECONOMICA', _('Econômica')
+        EXECUTIVA = 'EXECUTIVA', _('Executiva')
+        PRIMEIRA_CLASSE = 'PRIMEIRA_CLASSE', _('Primeira Classe')
