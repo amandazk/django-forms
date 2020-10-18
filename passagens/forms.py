44ceddb69d61afdda0f8543e21d1f2da136d4a6c
amandazk <zacharukaz@gmail.com>
2020-10-18 11:07:44 -0300

    ModelForm

--- passagens/forms.py
+++ passagens/forms.py
@@ -2,26 +2,26 @@ from django import forms
 from tempus_dominus.widgets import DatePicker
 from datetime import datetime
 from passagens.validation import *
+from passagens.models import Passagem, ClasseViagem, Pessoa
 
-class PassagemForms(forms.Form):
-    origem = forms.CharField(label='Origem', max_length=100)
-    destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida', widget=DatePicker())
-    data_volta = forms.DateField(label='Volta', widget=DatePicker())
+class PassagemForms(forms.ModelForm):
     data_pesquisa = forms.DateField(label='Data da pesquisa', disabled=True, initial=datetime.today) #a pessoa pode ver o campo, mas não pode alterá-lo
-    TIPOS_DE_CLASSE = {
-        (1, 'Econômica'),
-        (2, 'Executiva'),
-        (3, 'Primeira classe'),
-    }
-    classe_viagem = forms.ChoiceField(label='Classe da viagem', choices=TIPOS_DE_CLASSE)
-    informacoes = forms.CharField(
-        label='Informações extras',
-        max_length=200,
-        widget=forms.Textarea(),
-        required=False
-    )
-    email = forms.EmailField(label='Email', max_length=150)
+
+    class Meta:
+        model = Passagem
+        fields = '__all__'
+        labels = {
+            'data_ida': 'Data de ida', 
+            'data_volta': 'Data de volta', 
+            'informacoes': 'Informações',
+            'classe_viagem': 'Classe da viagem',
+            'data_pesquisa': 'Data da pesquisa'
+            }
+        widgets = {
+            'data_ida': DatePicker(),
+            'data_volta': DatePicker()
+        }
+    
 
     def clean(self):
         origem = self.cleaned_data.get('origem')
